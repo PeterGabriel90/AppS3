d2a8af54802355bf2f056df5107a4a2710ef11e9
Peter <Peter@DESKTOP-RG8Q9VD>
2021-10-24 21:22:25 -0500

    Agregar archivos de proyecto.

---
+++ AppS3/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppS3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string usuario = txtusuario.Text;
+            string contraseña = txtcontraseña.Text;
+            if ((usuario == "estudiante2021") && (contraseña == "uisrael2021"))
+            {
+                await Navigation.PushAsync(new Vista2(usuario, contraseña));
+
+            }
+            else
+            {
+                lblMensaje.Text = "El usuario o password son incorrectos";
+            }
+        }
+    }
+}
---
+++ AppS3/Vista2.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Vista2 : ContentPage
+    {
+        public Vista2(string usuario, string contraseña)
+        {
+            InitializeComponent();
+            lblUsuario.Text = "Usuario: " + usuario;
+            lblContraseña.Text = contraseña;
+        }
+
+        private void BtnVerificar_Clicked(object sender, EventArgs e)
+        {
+            double nota1 = Convert.ToDouble(txtValorS1.Text);
+            double exam1 = Convert.ToDouble(txtValorE1.Text);
+            double nota2 = Convert.ToDouble(txtValorS2.Text);
+            double exam2 = Convert.ToDouble(txtValorE2.Text);
+            //, examen1, nota2, examen2;
+            try
+            {
+                if (nota1 >= 0 && nota1 < 10.1 && exam1 >= 0 && exam1 < 10.01)
+                {
+                    if (nota2 >= 0 && nota2 < 10.1 && exam2 >= 0 && exam2 < 10.01)
+                    {
+                        double seg1 = nota1 * 0.3;
+                        double exa1 = exam1 * 0.2;
+                        double sumapar1 = seg1 + exa1;
+                        txtResultado1.Text = sumapar1.ToString();
+                        //
+                        double seg2 = nota2 * 0.3;
+                        double exa2 = exam2 * 0.2;
+                        double sumapar2 = seg2 + exa2;
+                        txtResultado2.Text = sumapar2.ToString();
+
+                        double notafin = sumapar1 + sumapar2;
+
+                        if (notafin >= 7)
+                        {
+                            txtNotaFin.Text = notafin.ToString() + " APROBADO";
+                        }
+                        else if (notafin >= 5 && notafin <= 6.99)
+                        {
+                            txtNotaFin.Text = notafin.ToString() + " COMPLEMENTARIO";
+                        }
+                        else
+                        {
+                            txtNotaFin.Text = notafin.ToString() + " REPROBADO";
+                        }
+                    }
+                    else
+                    {
+                        DisplayAlert("ERROR", "El " + nota2.ToString() + " sobrepasa el Valor", "ok");
+                    }
+                }
+                else
+                {
+                    DisplayAlert("ERROR", "El " + nota1.ToString() + " sobrepasa el Valor", "ok");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Mensaje de Alerta", ex.Message, "ok");
+            }
+        }
+    }
+}
